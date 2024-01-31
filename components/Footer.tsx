e55c904e695955a96b42bd7b2454272462aab654
szuluaga-beep <steven000069@hotmail.com>
2024-01-30 19:25:13 -0500

    Actualizando correo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -15,8 +15,8 @@ const Footer = () => {
             <p className='font-extrabold text-3xl'>*</p>
             <div className='flex flex-col items-center'>
                 <span className='uppercase font-extrabold text-lg'>+57 123 456 7890</span>
-                <a className='uppercase font-extrabold text-lg' href="mailto:chimbatours@tour.com">
-                    chimbatours@tour.com
+                <a className='uppercase font-extrabold text-lg' href="mailto:infochimbatours@gmail.com">
+                    infochimbatours@gmail.com
                 </a>
             </div>
         </div>
